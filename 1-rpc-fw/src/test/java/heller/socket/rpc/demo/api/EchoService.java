4bcb1ab66159a6a134bbdde3dd2435ee365da033
heller <heller@heller.com>
2021-04-24 02:32:52 +0800

    # 简单的 RPC 框架
    仿照 SPI 技术
    
    只实现了 本地 的 Socket 方式的服务

--- 1-rpc-fw/src/test/java/heller/socket/rpc/demo/api/EchoService.java
+++
@@ -1,5 +0,0 @@
-package heller.socket.rpc.demo.api;
-
-public interface EchoService {
-    String echo(String echo);
-}
--- 1-rpc-fw/src/test/java/heller/socket/rpc/demo/api/UploadService.java
+++
@@ -1,5 +0,0 @@
-package heller.socket.rpc.demo.api;
-
-public interface UploadService {
-    void upload(String filePath);
-}
